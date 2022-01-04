b3a1310d4da6b978bc74d87e8a1bb4813a0cb199
zinsmatt <zins.matthieu@gmail.com>
2022-01-04 12:32:19 +0100

    Accelerate CUDA version for MVS

--- ch12/dense_mono/kernels.cu
+++ ch12/dense_mono/kernels.cu
@@ -45,7 +45,7 @@ double norm2_cuda(const double in[2])
 }
 
 
-// inplace normalization
+// inplace normalization vec 3
 __device__
 inline void normalize3_cuda(double in_out[3]) {
     double d = sqrt(in_out[0]*in_out[0] 
@@ -56,7 +56,7 @@ inline void normalize3_cuda(double in_out[3]) {
     in_out[2] /= d;
 }
 
-
+// inplace normalization vec 2
 __device__
 inline void normalize2_cuda(double in_out[2]) {
     double d = sqrt(in_out[0]*in_out[0] + in_out[1]*in_out[1]);
@@ -65,11 +65,11 @@ inline void normalize2_cuda(double in_out[2]) {
 }
 
 __device__
-void transform_cuda(double x[3], const double T[3][4], double out[3])
+void transform_cuda(double x[3], const double T[12], double out[3])
 {
     for (int i = 0; i < 3; ++i)
     {
-        out[i] = x[0] * T[i][0] + x[1] * T[i][1] + x[2] * T[i][2] +  T[i][3];
+        out[i] = x[0] * T[i*4] + x[1] * T[i*4+1] + x[2] * T[i*4+2] +  T[i*4+3];
     }
 }
 
@@ -116,9 +116,9 @@ double ZNCC_cuda(const unsigned char *im1, const double pt1[2], const unsigned c
 
 __device__
 bool epipolar_search_cuda(const unsigned char* ref, const unsigned char* cur, 
-const double Tcr[3][4], const double pt[2],
-double depth_mu, double depth_sigma2, 
-double best_pc[2], double epipolar_dir[2])
+                          const double Tcr[12], const double pt[2],
+                          double depth_mu, double depth_sigma2, 
+                          double best_pc[2], double epipolar_dir[2])
 {
 
     double depth_sigma = sqrt(depth_sigma2);
@@ -206,7 +206,7 @@ void solve_Axb2_cuda(const double A[2][2], const double b[2], double res[2])
 }
 
 __device__
-void update_depth_filter_cuda(const double pr[2], const double pc[2], const double Trc[3][4], const double epipolar_dir[2], double *depth, double *cov2)
+void update_depth_filter_cuda(const double pr[2], const double pc[2], const double Trc[12], const double epipolar_dir[2], double *depth, double *cov2)
 {
     double fr[3];
     pix2cam_cuda(pr, fr);
@@ -216,12 +216,11 @@ void update_depth_filter_cuda(const double pr[2], const double pc[2], const doub
     pix2cam_cuda(pc, fc);
     normalize3_cuda(fc);
     
-    double f2[3] = {dot3_cuda(Trc[0], fc),
-                    dot3_cuda(Trc[1], fc),
-                    dot3_cuda(Trc[2], fc)};
+    double f2[3] = {dot3_cuda(Trc, fc),
+                    dot3_cuda(Trc+4, fc),
+                    dot3_cuda(Trc+8, fc)};
 
-    double trc[3] = {Trc[0][3], Trc[1][3], Trc[2][3]};
-    // printf(")))) %lf %lf %lf\n", trc[0], trc[1], trc[2]);
+    double trc[3] = {Trc[3], Trc[7], Trc[11]};
     double A[2][2];
     double b[2];
 
@@ -234,21 +233,18 @@ void update_depth_filter_cuda(const double pr[2], const double pc[2], const doub
     
     b[0] = dot3_cuda(fr, trc);
     b[1] = dot3_cuda(f2, trc);
-    // std::cout << det2_cuda(A) << "\n";
+
     if (abs(det2_cuda(A)) < 1e-20) // not invertible
         return;
 
-    // std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
     double res[2];
     solve_Axb2_cuda(A, b, res);
-    // std::cout << res[0] << " " << res[1]  << "\n";
     double P1[3] = {fr[0] * res[0], fr[1] * res[0], fr[2] * res[0]};
     double P2[3] = {trc[0] + fc[0] * res[1], trc[1] + fc[1] * res[1], trc[2] + fc[2] * res[1]};
     double P_est[3] = {(P1[0] + P2[0]) * 0.5, 
                        (P1[1] + P2[1]) * 0.5, 
                        (P1[2] + P2[2]) * 0.5};
     double depth_obs = norm3_cuda(P_est);
-    // printf("depth = %lf\n", depth_obs);
 
     double P[3] = {fr[0] * depth_obs, fr[1] * depth_obs, fr[2] * depth_obs};
     double a[3] = {P[0] - trc[0], P[1] - trc[1], P[2] - trc[2]};
@@ -266,13 +262,7 @@ void update_depth_filter_cuda(const double pr[2], const double pc[2], const doub
     double beta_2 = acos(-dot3_cuda(fc2, t));
 
     double gamma = M_PI - alpha - beta_2;
-    // printf(")))) %lf %lf %lf\n", trc[0]*trc[0], trc[1]*trc[1], trc[2]*trc[2]);
-    // printf("]]] %lf %lf", trc[0]*trc[0]+trc[1]*trc[1]+trc[2]*trc[2], sqrt(trc[0]*trc[0]+trc[1]*trc[1]+trc[2]*trc[2]));
-    // printf(")))) %lf %lf %lf\n", trc[0], trc[1], trc[2]);
-
-    // printf("==> %lf %lf %lf => %lf\n", norm3_cuda(trc), beta_2, gamma, sin(beta_2) / sin(gamma));
     double d_noise = norm3_cuda(trc) * sin(beta_2) / sin(gamma); // sinus law
-    // printf("dnoise = %lf\n", d_noise);
     double sigma_obs = depth_obs - d_noise;
     double sigma2_obs = sigma_obs * sigma_obs;
 
@@ -284,90 +274,44 @@ void update_depth_filter_cuda(const double pr[2], const double pc[2], const doub
     double d_fused = (sigma2_obs * d + sigma2 * depth_obs) / (sigma2 + sigma2_obs);
     double sigma2_fused = (sigma2 * sigma2_obs) / (sigma2 + sigma2_obs);
 
-    // debug << d_fused << "\n";
     depth[(int)pr[1] * width + (int)pr[0]] = d_fused;
     cov2[(int)pr[1] * width + (int)pr[0]] = sigma2_fused;
-    // printf(")))) %lf %lf %lf\n", trc[0], trc[1], trc[2]);
 
-    if (isnan(d_fused))
-    {
-        printf("%lf, %lf, %lf\n", d_fused, sigma2, sigma2_obs);
-        printf(")))) %lf %lf %lf\n", trc[0], trc[1], trc[2]);
-
-        printf("]]] %lf %lf\n", trc[0]*trc[0]+trc[1]*trc[1]+trc[2]*trc[2], sqrt(trc[0]*trc[0]+trc[1]*trc[1]+trc[2]*trc[2]));
-        printf("==> %lf %lf %lf => %lf\n", norm3_cuda(trc), beta_2, gamma, sin(beta_2) / sin(gamma));
-        printf("d sigma2 %lf %lf\n", d, sigma2);
-        printf("depth obs sigma2 obs %lf %lf\n", depth_obs, sigma2_obs);
-        printf("P_est  %lf %lf %lf\n", P_est[0], P_est[1], P_est[2]);
-        printf("res %lf %lf\n", res[0], res[1]);
-        printf("fr  %lf %lf %lf\n", fr[0], fr[1], fr[2]);
-        printf("f2  %lf %lf %lf\n", f2[0], f2[1], f2[2]);
-        printf("pr  %lf %lf %lf\n", pr[0], pr[1], pr[2]);
-        printf("pc  %lf %lf %lf\n", pc[0], pc[1], pc[2]);
-
-    }
 }
 
 
+__device__ double Tcr_global[12];
+__device__ double Trc_global[12];
 
 
 __global__
-void process_pixel_cuda(int i, int j, const unsigned char* ref, const unsigned char* cur, Mat3x4 Tcr, Mat3x4 Trc,  double *depth, double *cov2)
-// void process_pixel_cuda(int i, int j, const unsigned char* ref, const unsigned char* cur, double Tcr_[3][4], double Trc_[3][4],  double *depth, double *cov2, unsigned char *status)
+void process_pixel_cuda(const unsigned char* ref, const unsigned char* cur, double *depth, double *cov2)
 {
-    j = boarder + blockIdx.x;
-    i = boarder + blockIdx.y;
+
+    int j = boarder + (blockIdx.x * blockDim.x) + threadIdx.x;
+    int i = boarder + (blockIdx.y * blockDim.y) + threadIdx.y;
 
     double depth_mu = depth[i*width+j];
     double depth_sigma2 = cov2[i*width+j];
 
-    double Tcr_[3][4];
-    Tcr_[0][0] = Tcr.a00;
-    Tcr_[0][1] = Tcr.a01;
-    Tcr_[0][2] = Tcr.a02;
-    Tcr_[0][3] = Tcr.a03;
-    Tcr_[1][0] = Tcr.a10;
-    Tcr_[1][1] = Tcr.a11;
-    Tcr_[1][2] = Tcr.a12;
-    Tcr_[1][3] = Tcr.a13;
-    Tcr_[2][0] = Tcr.a20;
-    Tcr_[2][1] = Tcr.a21;
-    Tcr_[2][2] = Tcr.a22;
-    Tcr_[2][3] = Tcr.a23;
-
-    double Trc_[3][4];
-    Trc_[0][0] = Trc.a00;
-    Trc_[0][1] = Trc.a01;
-    Trc_[0][2] = Trc.a02;
-    Trc_[0][3] = Trc.a03;
-    Trc_[1][0] = Trc.a10;
-    Trc_[1][1] = Trc.a11;
-    Trc_[1][2] = Trc.a12;
-    Trc_[1][3] = Trc.a13;
-    Trc_[2][0] = Trc.a20;
-    Trc_[2][1] = Trc.a21;
-    Trc_[2][2] = Trc.a22;
-    Trc_[2][3] = Trc.a23;
-
     if (depth_sigma2 < min_cov || depth_sigma2 > max_cov)
         return;
 
-    double pr[2] = {j, i};
+    double pr[2] = {(double)j, (double)i};
     double pc[2];
     double epipolar_dir[2];
 
-    bool found = epipolar_search_cuda(ref, cur, Tcr_, pr, depth_mu, depth_sigma2, pc, epipolar_dir);
+    bool found = epipolar_search_cuda(ref, cur, Tcr_global, pr, depth_mu, depth_sigma2, pc, epipolar_dir);
     
     if (!found)
         return;
 
-    update_depth_filter_cuda(pr, pc, Trc_, epipolar_dir, depth, cov2);
+    update_depth_filter_cuda(pr, pc, Trc_global, epipolar_dir, depth, cov2);
 }
 
 
 void wrapper_update_cuda(const unsigned char* ref, const unsigned char* cur, double Tcr[3][4], double Trc[3][4], double *depth, double *cov2)
 {
-    // std::cout << "=================================================================\n";
 
     size_t size_uchar = sizeof(unsigned char) * width * height;
     size_t size_double = sizeof(double) * width * height;
@@ -388,39 +332,18 @@ void wrapper_update_cuda(const unsigned char* ref, const unsigned char* cur, dou
 
     int A = 480 - 2 * boarder; // height
     int B = 640 - 2 * boarder; // width
-    dim3 grid_dim(B, A);
-    // dim3 grid_dim(1, 1);
-    Mat3x4 Tcr_;
-    Tcr_.a00 = Tcr[0][0];
-    Tcr_.a01 = Tcr[0][1];
-    Tcr_.a02 = Tcr[0][2];
-    Tcr_.a03 = Tcr[0][3];
-    Tcr_.a10 = Tcr[1][0];
-    Tcr_.a11 = Tcr[1][1];
-    Tcr_.a12 = Tcr[1][2];
-    Tcr_.a13 = Tcr[1][3];
-    Tcr_.a20 = Tcr[2][0];
-    Tcr_.a21 = Tcr[2][1];
-    Tcr_.a22 = Tcr[2][2];
-    Tcr_.a23 = Tcr[2][3];
 
-    Mat3x4 Trc_;
-    Trc_.a00 = Trc[0][0];
-    Trc_.a01 = Trc[0][1];
-    Trc_.a02 = Trc[0][2];
-    Trc_.a03 = Trc[0][3];
-    Trc_.a10 = Trc[1][0];
-    Trc_.a11 = Trc[1][1];
-    Trc_.a12 = Trc[1][2];
-    Trc_.a13 = Trc[1][3];
-    Trc_.a20 = Trc[2][0];
-    Trc_.a21 = Trc[2][1];
-    Trc_.a22 = Trc[2][2];
-    Trc_.a23 = Trc[2][3];
+    dim3 block_dim(16, 16);
+    dim3 grid_dim(B / 16 + 1, A / 16 + 1);
+    // std::cout << "grid_dim " << grid_dim.x << " " << grid_dim.y << "\n";
+    // std::cout << "block_dim " << block_dim.x << " " << block_dim.y << "\n";
+   
 
+    cudaMemcpyToSymbol(Tcr_global, &Tcr[0][0], 12 * sizeof(double), 0, cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(Trc_global, &Trc[0][0], 12 * sizeof(double), 0, cudaMemcpyHostToDevice);
 
-
-    process_pixel_cuda<<<grid_dim, 1>>>(0, 0, ref_cuda, cur_cuda, Tcr_, Trc_, depth_cuda, cov2_cuda);
+    process_pixel_cuda<<<grid_dim, block_dim>>>(ref_cuda, cur_cuda, depth_cuda, cov2_cuda);
+    std::cout << cudaGetErrorName(cudaPeekAtLastError()) << std::endl;
     cudaDeviceSynchronize();
 
 
